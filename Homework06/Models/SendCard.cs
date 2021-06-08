e7836ace59aa22c7e1666a539f0bbd88b52ac8a0
Glauber Marques <75267767+gvfmarques@users.noreply.github.com>
2021-06-07 20:12:02 -0500

    Add project files.

---
+++ Homework06/Models/SendCard.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Homework06.Models
+{
+    public class SendCard
+    {
+        [Required]
+        public string From { get; set; }
+        [Required]
+        public string To { get; set; }
+        [Required]
+        public string Message { get; set; }
+    }
+}
